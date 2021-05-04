06bf7184adcafb6cfc40126096c6f0ee0cf18a66
Matias <matias.marcelo.calvo@gmail.com>
2021-05-04 01:08:28 -0300

    Colors & ErrorsKeys constants fiels created

---
+++ constants/Colors.js
@@ -0,0 +1,17 @@
+const black = '#000';
+const gray = '#808080';
+const lightGray = '#EEE';
+const primaryColor = '#0490E1';
+const red = '#E74C3C';
+const secondaryColor = '#00FFCE';
+const white = '#FFF';
+
+export default {
+  black,
+  gray,
+  lightGray,
+  primaryColor,
+  red,
+  secondaryColor,
+  white
+};
---
+++ constants/ErrorsKeys.js
@@ -0,0 +1,15 @@
+export default {
+  DEFAULT_MESSAGE: 'Something Went Wrong',
+  // COMMON
+  INVALID_REQUEST: 'Invalid Request',
+  ZERO_RESULTS: 'Zero Results',
+  OVER_QUERY_LIMIT: 'Over Query Limit',
+  REQUEST_DENIED: 'Request Denied',
+  UNKNOWN_ERROR: 'Unknown Error',
+
+  // DIRECTIONS
+  MAX_ROUTE_LENGTH_EXCEEDED: 'Max Route Length Exceeded',
+  MAX_WAYPOINTS_EXCEEDED: 'Max Waypoints Exceeded',
+  NOT_FOUND: 'Not Found',
+  OVER_DAILY_LIMIT: 'Over Daily Limit'
+};
